4336c95a106fa161576d8b2b3ac66113ed270da4
Yota <106178330+sunThick@users.noreply.github.com>
2023-04-07 18:48:26 +0900

    home-tabbar

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -5,16 +5,59 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Image.asset(
-          'assets/images/homeLogo.png',
-          fit: BoxFit.contain,
-          height: 35,
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 8,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Image.asset(
+            'assets/images/homeLogo.png',
+            fit: BoxFit.contain,
+            height: 35,
+          ),
+          bottom: const TabBar(
+            isScrollable: true,
+            tabs: <Widget>[
+              Tab(text: 'all'),
+              Tab(text: 'single'),
+              Tab(text: 'MTB'),
+              Tab(text: 'touring'),
+              Tab(text: 'road'),
+              Tab(text: 'mini'),
+              Tab(text: 'mamachari'),
+              Tab(text: 'others'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            Center(
+              child: Text('all', style: TextStyle(fontSize: 32.0)),
+            ),
+            Center(
+              child: Text('single', style: TextStyle(fontSize: 32.0)),
+            ),
+            Center(
+              child: Text('MTB', style: TextStyle(fontSize: 32.0)),
+            ),
+            Center(
+              child: Text('touring', style: TextStyle(fontSize: 32.0)),
+            ),
+            Center(
+              child: Text('road', style: TextStyle(fontSize: 32.0)),
+            ),
+            Center(
+              child: Text('mini', style: TextStyle(fontSize: 32.0)),
+            ),
+            Center(
+              child: Text('mamachari', style: TextStyle(fontSize: 32.0)),
+            ),
+            Center(
+              child: Text('others', style: TextStyle(fontSize: 32.0)),
+            ),
+          ],
         ),
       ),
-      body:
-          const Center(child: Text('home画面', style: TextStyle(fontSize: 32.0))),
     );
   }
 }
