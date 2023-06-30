25d67be7939a55b7c63dea2753403aaa8c209847
Yota <106178330+sunThick@users.noreply.github.com>
2023-06-30 21:52:59 +0900

    muteuserを除外する関数を追加

--- lib/constants/void.dart
+++ lib/constants/void.dart
@@ -1,19 +1,37 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:yourchari_app/domain/firestore_user/firestore_user.dart';
 import 'package:yourchari_app/viewModels/main_controller.dart';
 
 import '../domain/chari/chari.dart';
 
-List<QueryDocumentSnapshot<Map<String, dynamic>>> chariWithoutMuteUser(
-    {required List<QueryDocumentSnapshot<Map<String, dynamic>>> chariDocs,
+List<DocumentSnapshot<Map<String, dynamic>>> chariWithoutMuteUser(
+    {required List<DocumentSnapshot<Map<String, dynamic>>> chariDocs,
     required MainController mainController}) {
-  final List<QueryDocumentSnapshot<Map<String, dynamic>>>
+  final List<DocumentSnapshot<Map<String, dynamic>>>
       chariWithoutMuteUserDocs = [];
   final List<String> muteUids = mainController.muteUids;
   for (final chariDoc in chariDocs) {
-    final Chari chari = Chari.fromJson(chariDoc.data());
+    final Chari chari = Chari.fromJson(chariDoc.data()!);
     if (!muteUids.contains(chari.uid)) {
       chariWithoutMuteUserDocs.add(chariDoc);
     }
   }
   return chariWithoutMuteUserDocs;
+}
+
+List<DocumentSnapshot<Map<String, dynamic>>> userListWithoutMuteUser(
+    {required List<DocumentSnapshot<Map<String, dynamic>>> userDocs,
+    required MainController mainController}) {
+  final List<DocumentSnapshot<Map<String, dynamic>>>
+      userListWithoutMuteUserDocs = [];
+  for (final userDoc in userDocs) {
+    if (userDoc.data() != null) {
+      final FirestoreUser firestoreUser =
+          FirestoreUser.fromJson(userDoc.data()!);
+      if (!mainController.muteUids.contains(firestoreUser.uid)) {
+        userListWithoutMuteUserDocs.add(userDoc);
+      }
+    }
+  }
+  return userListWithoutMuteUserDocs;
 }
